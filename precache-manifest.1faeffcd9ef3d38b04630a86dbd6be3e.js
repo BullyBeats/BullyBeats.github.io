d82847b471346abd6e3e3ffe529370357ae25c2e
Manish Dalal <manish.d@bettertechsolution.com>
2021-02-07 22:09:44 +0530

    Updates

---
+++ precache-manifest.1faeffcd9ef3d38b04630a86dbd6be3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c448efa740e9cf60102ebeee116136a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "909a839d228e6d947242",
+    "url": "/static/css/2.fe51dd2d.chunk.css"
+  },
+  {
+    "revision": "847fe993ac323da64fda",
+    "url": "/static/css/main.2ca1c46a.chunk.css"
+  },
+  {
+    "revision": "909a839d228e6d947242",
+    "url": "/static/js/2.b8064d92.chunk.js"
+  },
+  {
+    "revision": "9a2cf355ca348d84eedb0129142dc576",
+    "url": "/static/js/2.b8064d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847fe993ac323da64fda",
+    "url": "/static/js/main.483db6ee.chunk.js"
+  },
+  {
+    "revision": "0da1f4af1f9d9fe1ae46",
+    "url": "/static/js/runtime-main.22e9eeb1.js"
+  },
+  {
+    "revision": "b41f0c57c6dd4ea7a68adbe4e74bc70e",
+    "url": "/static/media/home-bg-img.b41f0c57.jpg"
+  },
+  {
+    "revision": "471a1ad342659289433e05a611d206f8",
+    "url": "/static/media/user-logo.471a1ad3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e282e0e0f673676ddc0316058278326c.js"
+  "/precache-manifest.1faeffcd9ef3d38b04630a86dbd6be3e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
